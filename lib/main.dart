40a580c644a0e1a22434fb0aac7e50f6de8c69b9
JeffreyDinackus <jeffreydinackus@proton.me>
2023-07-13 15:50:09 -0400

    Release 1.0 is out. May not be bug free.

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,217 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(TrackerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class TrackerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tracker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TrackerHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TrackerHomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TrackerHomePageState createState() => _TrackerHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TrackerHomePageState extends State<TrackerHomePage> {
+  List<Tracker> trackers = [];
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _loadTrackers();
+  }
+
+  Future<void> _loadTrackers() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? trackerList = prefs.getStringList('trackers');
+
+    if (trackerList != null) {
+      List<Tracker> loadedTrackers = trackerList.map((trackerString) {
+        List<String> parts = trackerString.split(':');
+        String label = parts[0];
+        int count = int.parse(parts[1]);
+        return Tracker(label: label, count: count);
+      }).toList();
+
+      setState(() {
+        trackers = loadedTrackers;
+      });
+    }
+  }
+
+  Future<void> _saveTrackers() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> trackerList =
+        trackers.map((tracker) => '${tracker.label}:${tracker.count}').toList();
+    await prefs.setStringList('trackers', trackerList);
+  }
+
+  void _addTracker() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      trackers = List.from(trackers)..add(Tracker());
     });
+    _saveTrackers();
+  }
+
+  void _removeTracker(int index) {
+    setState(() {
+      trackers = List.from(trackers)..removeAt(index);
+    });
+    _saveTrackers();
+  }
+
+  void _incrementCount(int index) {
+    setState(() {
+      trackers = List.from(trackers)..[index].count += 1;
+    });
+    _saveTrackers();
+  }
+
+  void _decrementCount(int index) {
+    setState(() {
+      trackers = List.from(trackers)..[index].count -= 1;
+    });
+    _saveTrackers();
+  }
+
+  void _updateTrackerLabel(int index, String label) {
+    setState(() {
+      trackers[index].label = label;
+    });
+    _saveTrackers();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Tracker'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: ListView.builder(
+        itemCount: trackers.length,
+        itemBuilder: (context, index) {
+          return TrackerCard(
+            tracker: trackers[index],
+            onRemove: () => _removeTracker(index),
+            onIncrement: () => _incrementCount(index),
+            onDecrement: () => _decrementCount(index),
+            onUpdateLabel: (label) => _updateTrackerLabel(index, label),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addTracker,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class Tracker {
+  String label;
+  int count;
+
+  Tracker({this.label = 'Count', this.count = 0});
+}
+
+class TrackerCard extends StatefulWidget {
+  final Tracker tracker;
+  final VoidCallback onRemove;
+  final VoidCallback onIncrement;
+  final VoidCallback onDecrement;
+  final ValueChanged<String> onUpdateLabel;
+
+  TrackerCard({
+    required this.tracker,
+    required this.onRemove,
+    required this.onIncrement,
+    required this.onDecrement,
+    required this.onUpdateLabel,
+  });
+
+  @override
+  _TrackerCardState createState() => _TrackerCardState();
+}
+
+class _TrackerCardState extends State<TrackerCard> {
+  TextEditingController _labelController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _labelController.text = widget.tracker.label;
+  }
+
+  @override
+  void dispose() {
+    _labelController.dispose();
+    super.dispose();
+  }
+
+  void _saveLabel() {
+    widget.onUpdateLabel(_labelController.text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Padding(
+        padding: EdgeInsets.all(16),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  child: TextField(
+                    controller: _labelController,
+                    decoration: InputDecoration(
+                      labelText: 'Label',
+                    ),
+                    onChanged: (value) => _saveLabel(),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: widget.onRemove,
+                ),
+              ],
             ),
+            SizedBox(height: 16),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Count: ${widget.tracker.count}',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: widget.onDecrement,
+                  child: Icon(Icons.remove),
+                ),
+                ElevatedButton(
+                  onPressed: widget.onIncrement,
+                  child: Icon(Icons.add),
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
